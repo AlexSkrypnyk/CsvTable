026c6263bc67f7d7a6e0201f05cc34139fd7a43a
Alex Skrypnyk <alex@drevops.com>
2025-10-27 12:13:18 +1100

    Fixed max columns width calculation and coding standards.

--- tests/phpunit/TestFormatter.php
+++ tests/phpunit/TestFormatter.php
@@ -34,7 +34,7 @@ class TestFormatter {
       $output .= "\n" . str_repeat('=', strlen($output)) . "\n";
     }
 
-    return $output . implode("\n", array_map(static function ($row) use ($options): string {
+    return $output . implode("\n", array_map(static function (array $row) use ($options): string {
         return implode($options['delimiter'], $row);
     }, $rows));
   }
